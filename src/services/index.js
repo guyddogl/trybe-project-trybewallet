ac61f5e1bba06be4c3ceea2c3dbaf3d3d24a21e5
Guyddo <guyddogl@gmail.com>
2022-08-26 20:46:51 -0300

    Change API return

--- src/services/index.js
+++ src/services/index.js
@@ -3,7 +3,7 @@ const URL_API = 'https://economia.awesomeapi.com.br/json/all';
 async function getCurrencies() {
   const response = await fetch(URL_API);
   const currency = await response.json();
-  return Object.entries(currency);
+  return currency;
 }
 
 export default getCurrencies;
