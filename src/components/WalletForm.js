33c8997bc503cbad86f51427d20f2dc65bdeb6f0
Guyddo <guyddogl@gmail.com>
2022-08-26 18:53:04 -0300

    Create input and select tags

--- src/components/WalletForm.js
+++ src/components/WalletForm.js
@@ -1,9 +1,51 @@
 import React, { Component } from 'react';
+import getCurrency from '../services';
 
 class WalletForm extends Component {
+  state = {
+    despesa: '',
+    currencies: [],
+  };
+
+  async componentDidMount() {
+    const api = await getCurrency();
+    this.setState({
+      currencies: api,
+    });
+  }
+
+  handleChange = ({ target: { name, value } }) => {
+    this.setState({
+      [name]: value,
+    });
+  };
+
   render() {
+    const { despesa } = this.state;
+    const { currencies } = this.state;
     return (
-      <div>WalletForm</div>
+      <section className="container-fluid">
+        <div className="row justify-content-center align-items-center">
+          <input
+            type="text"
+            name="despesa"
+            value={ despesa }
+            placeholder="Despesa"
+            className="form-control my-3"
+            onChange={ this.handleChange }
+            data-testid="value-input"
+          />
+          <select
+            className="form-select"
+            data-testid="currency-input"
+          >
+            {currencies.map((currency) => (
+              currency[0] !== 'USDT'
+              && <option key={ currency[0] } value={ currency[0] }>{currency[0]}</option>
+            ))}
+          </select>
+        </div>
+      </section>
     );
   }
 }
