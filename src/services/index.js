a620045e98a2ba2e9a0e8bf9953cd3e684330e75
Guyddo <guyddogl@gmail.com>
2022-08-26 19:54:29 -0300

    Change const name

--- src/services/index.js
+++ src/services/index.js
@@ -1,9 +1,9 @@
-const GET_CURRENCY = 'https://economia.awesomeapi.com.br/json/all';
+const URL_API = 'https://economia.awesomeapi.com.br/json/all';
 
-async function getCurrency() {
-  const response = await fetch(GET_CURRENCY);
+async function getCurrencies() {
+  const response = await fetch(URL_API);
   const currency = await response.json();
   return Object.entries(currency);
 }
 
-export default getCurrency;
+export default getCurrencies;
