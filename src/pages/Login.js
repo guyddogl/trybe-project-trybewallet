371596d94aab47158f937c1164d49ee45582629e
Guyddo <guyddogl@gmail.com>
2022-08-26 14:49:26 -0300

    Creating login form

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,41 @@
 import React from 'react';
 
 class Login extends React.Component {
+  state = {
+    email: '',
+    senha: '',
+    isButtonDisabled: true,
+  };
+
+  handleChange = () => {
+    console.log('change');
+  };
+
+  handleSubmit = () => {
+    console.log('Entrou');
+  };
+
   render() {
-    return <div>Login</div>;
+    const { isButtonDisabled, email, senha } = this.state;
+    return (
+      <form onSubmit={ this.handleSubmit }>
+        <input
+          type="email"
+          name={ email }
+          placeholder="E-mail"
+          onChange={ this.handleChange }
+          data-testid="email-input"
+        />
+        <input
+          type="password"
+          name={ senha }
+          placeholder="Senha"
+          onChange={ this.handleChange }
+          data-testid="password-input"
+        />
+        <button type="submit" disabled={ isButtonDisabled }>Entrar</button>
+      </form>
+    );
   }
 }
 
