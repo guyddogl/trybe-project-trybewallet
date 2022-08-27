6edf6c3d628b24088ac2551da948b24b67c50472
Guyddo <guyddogl@gmail.com>
2022-08-27 19:45:52 -0300

    Create action actionSetEditorFalse

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -5,6 +5,7 @@ export const GET_CURRENCIES = 'GET_CURRENCIES';
 export const ADD_EXPENSE = 'ADD_EXPENSE';
 export const DELETE_EXPENSE = 'DELETE_EXPENSE';
 export const EDIT_EXPENSE = 'EDIT_EXPENSE';
+export const SET_EDITOR_FALSE = 'SET_EDITOR_FALSE';
 
 export const actionSaveUser = (payload) => ({ type: SAVE_USER, payload });
 
@@ -18,3 +19,4 @@ export const getCurrenciesThunk = () => async (dispatch) => {
 export const actionAddExpense = (payload) => ({ type: ADD_EXPENSE, payload });
 export const actionDeleteExpense = (payload) => ({ type: DELETE_EXPENSE, payload });
 export const actionEditExpense = (payload) => ({ type: EDIT_EXPENSE, payload });
+export const actionSetEditorFalse = (payload) => ({ type: SET_EDITOR_FALSE, payload });
