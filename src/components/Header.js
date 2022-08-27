0ca48c8a40f11c30cf88621760a3ce5d85d6933f
Guyddo <guyddogl@gmail.com>
2022-08-27 12:04:56 -0300

    Sum expense values

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,12 +3,22 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
 class Header extends Component {
+  sumExpensesValues = ({ expenses } = this.props) => {
+    let sumValues = 0;
+    expenses.forEach((expense) => {
+      const expenseValue = expense.value;
+      const askValue = expense.exchangeRates[expense.currency].ask;
+      sumValues += expenseValue * askValue;
+    });
+    return sumValues.toFixed(2);
+  };
+
   render() {
     const { email } = this.props;
     return (
       <div>
         <p data-testid="email-field">{email}</p>
-        <p data-testid="total-field">0</p>
+        <p data-testid="total-field">{this.sumExpensesValues()}</p>
         <p data-testid="header-currency-field">BRL</p>
       </div>
     );
@@ -17,10 +27,12 @@ class Header extends Component {
 
 const mapStateToProps = (state) => ({
   email: state.user.email,
+  expenses: state.wallet.expenses,
 });
 
 Header.propTypes = {
   email: PropTypes.string.isRequired,
+  // expenses: PropTypes.arrayOf(PropTypes.shape({ }).isRequired).isRequired,
 };
 
 export default connect(mapStateToProps)(Header);
