36d4c2dc6238f3a803f3277a2ec33446afe6fc7d
Guyddo <guyddogl@gmail.com>
2022-08-27 19:14:51 -0300

    Create action actionEditExpense

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -4,6 +4,7 @@ export const SAVE_USER = 'SAVE_USER';
 export const GET_CURRENCIES = 'GET_CURRENCIES';
 export const ADD_EXPENSE = 'ADD_EXPENSE';
 export const DELETE_EXPENSE = 'DELETE_EXPENSE';
+export const EDIT_EXPENSE = 'EDIT_EXPENSE';
 
 export const actionSaveUser = (payload) => ({ type: SAVE_USER, payload });
 
@@ -16,3 +17,4 @@ export const getCurrenciesThunk = () => async (dispatch) => {
 
 export const actionAddExpense = (payload) => ({ type: ADD_EXPENSE, payload });
 export const actionDeleteExpense = (payload) => ({ type: DELETE_EXPENSE, payload });
+export const actionEditExpense = (payload) => ({ type: EDIT_EXPENSE, payload });
