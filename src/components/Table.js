84b1c8f871dd84a13fd7dbb94c833a6febdb457f
Guyddo <guyddogl@gmail.com>
2022-08-27 13:20:55 -0300

    Requirement 6. Create table header

--- src/components/Table.js
+++ src/components/Table.js
@@ -3,7 +3,21 @@ import React, { Component } from 'react';
 class Table extends Component {
   render() {
     return (
-      <div>Table</div>
+      <table className="table table-hover table-bordered">
+        <thead className="table-primary text-center">
+          <tr>
+            <th scope="col">Descrição</th>
+            <th scope="col">Tag</th>
+            <th scope="col">Método de pagamento</th>
+            <th scope="col">Valor</th>
+            <th scope="col">Moeda</th>
+            <th scope="col">Câmbio utilizado</th>
+            <th scope="col">Valor convertido</th>
+            <th scope="col">Moeda de conversão</th>
+            <th scope="col">Editar/Excluir</th>
+          </tr>
+        </thead>
+      </table>
     );
   }
 }
